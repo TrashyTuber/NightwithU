7e2cfacac0a7b0bfb433e3069c7778401dede852
TrashyTuber <jasonjiaym@gmail.com>
2025-12-30 23:39:04 -0800

    Added a scroll detection to create a transition on NavBar utilizing useEffect

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
 import './NavBar.css'
 
 function NavBar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav>
+    <nav className={scrolled ? 'scrolled-nav' : ''}>
       <div className="nav-container">
-        
+                
         <div className="nav-left">
           <a href="#home">Home</a>
           <a href="#explore">Explore</a>
